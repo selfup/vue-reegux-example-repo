2796902798f228b67618b0eabefb5f0046d212ca
Regis <boudinot.regis@yahoo.com>
2016-09-19 15:16:32 -0600

    update vue-reegux to new non-breaking scope - formatting

--- src/controllers/tom/index.js
+++ src/controllers/tom/index.js
@@ -2,4 +2,4 @@ import tomState from './store'
 import tomActions from './actions'
 import { bindActionsToStore } from 'vue-reegux'
 
- export default bindActionsToStore(tomState, tomActions)
+export default bindActionsToStore(tomState, tomActions)
